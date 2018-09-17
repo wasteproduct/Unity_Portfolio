ae016a4552ac754dec09bfbfd537e666d3f54e87
wasteproduct <heilmittelh@naver.com>
2018-09-17 17:12:12 +0900

    180917_1

--- Portfolio/Assets/Scripts/Camera_Movement.cs
+++ Portfolio/Assets/Scripts/Camera_Movement.cs
@@ -2,21 +2,18 @@
 
 public class Camera_Movement : MonoBehaviour
 {
-    public Transform player;
-
-    private Vector3 offset;
+    public Transform focus;
+    public Vector3 offset;
 
     // Use this for initialization
     void Start()
     {
-        offset = new Vector3(-16.0f, 16.0f, -16.0f);
-
         this.transform.rotation = Quaternion.Euler(30.0f, 45.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = player.position + offset;
+        this.transform.position = focus.position + offset;
     }
 }
