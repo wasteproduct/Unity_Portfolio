83e2cab98878281d6c9ce2acce50017330bd0e39
wasteproduct <heilmittelh@naver.com>
2018-09-17 19:26:53 +0900

    180917_2

--- Portfolio/Assets/Scripts/Player_Main.cs
+++ Portfolio/Assets/Scripts/Player_Main.cs
@@ -1,26 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Player
 {
-    public class Player_Main : MonoBehaviour
+    public class Player_Main : ScriptableObject
     {
-        public GameObject soldier;
-        public Player_Team team;
 
-        // Use this for initialization
-        void Start()
-        {
-            GameObject newSoldier = Instantiate<GameObject>(soldier, this.gameObject.transform);
-            team.AddTeamMember(newSoldier.GetComponent<PlayerCharacter_Soldier>());
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                print(team.TeamMembers.Count);
-            }
-        }
     }
 }
---
+++ Portfolio/Assets/Scripts/Player_TemporaryMain.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class Player_TemporaryMain : MonoBehaviour
+    {
+        public GameObject soldier;
+        public Player_Team team;
+
+        // Use this for initialization
+        void Start()
+        {
+            GameObject newSoldier = Instantiate<GameObject>(soldier, this.gameObject.transform);
+            team.AddTeamMember(newSoldier.GetComponent<PlayerCharacter_Soldier>());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                print(team.TeamMembers.Count);
+            }
+        }
+    }
+}
