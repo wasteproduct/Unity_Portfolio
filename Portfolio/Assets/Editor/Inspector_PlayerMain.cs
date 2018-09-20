b4ef6023f995c33ff478df6b8c6774e0cb8f63d6
wasteproduct <heilmittelh@naver.com>
2018-09-20 17:25:02 +0900

    180920_1

---
+++ Portfolio/Assets/Editor/Inspector_PlayerMain.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Player;
+
+[CustomEditor(typeof(Player_Main))]
+public class Inspector_PlayerMain : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Add Characters"))
+        {
+            Player_Main playerMain = (Player_Main)target;
+            playerMain.AddCharacters();
+        }
+
+        if (GUILayout.Button("Clear Characters"))
+        {
+            Player_Main playerMain = (Player_Main)target;
+            playerMain.ClearCharacters();
+        }
+
+        if (GUILayout.Button("Print Character Information"))
+        {
+            Player_Main playerMain = (Player_Main)target;
+            playerMain.PrintCharacterInformation();
+        }
+
+        if (GUILayout.Button("Increase Strength"))
+        {
+            Player_Main playerMain = (Player_Main)target;
+            playerMain.IncreaseStrength();
+        }
+    }
+}
--- Portfolio/Assets/Scripts/Player_Main.cs
+++ Portfolio/Assets/Scripts/Player_Main.cs
@@ -5,8 +5,12 @@ using Character;
 
 namespace Player
 {
+    [CreateAssetMenu(fileName = "", menuName = "Player/Main", order = 1)]
     public class Player_Main : ScriptableObject
     {
+        public Character_Base soldierBase;
+        public Character_Base tankBase;
+
         private List<Character_Base> characters = new List<Character_Base>();
 
         public List<Character_Base> Characters { get { return characters; } }
@@ -16,7 +20,34 @@ namespace Player
             if (characters.Contains(newCharacter) == false) characters.Add(newCharacter);
         }
 
-        private void OnDisable()
+
+        // temporary
+        // 여기
+        public int listIndex;
+
+        public void PrintCharacterInformation()
+        {
+
+        }
+
+        public void IncreaseStrength()
+        {
+
+        }
+
+        public void AddCharacters()
+        {
+            Character_Base newSoldier = ScriptableObject.CreateInstance<Character_Base>();
+            newSoldier = soldierBase;
+
+            Character_Base newTank = ScriptableObject.CreateInstance<Character_Base>();
+            newTank = tankBase;
+
+            characters.Add(newSoldier);
+            characters.Add(newTank);
+        }
+        
+        public void ClearCharacters()
         {
             for (int i = characters.Count - 1; i >= 0; i--)
             {
