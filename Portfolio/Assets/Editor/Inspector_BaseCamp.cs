e513320a141c17a98a64ee9451b93ac3e601b71a
wasteproduct <heilmittelh@naver.com>
2018-09-21 17:17:34 +0900

    180921_1

---
+++ Portfolio/Assets/Editor/Inspector_BaseCamp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Manager_BaseCamp))]
+public class Inspector_BaseCamp : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Characters"))
+        {
+
+        }
+
+        if (GUILayout.Button("Organize Team"))
+        {
+
+        }
+    }
+}
