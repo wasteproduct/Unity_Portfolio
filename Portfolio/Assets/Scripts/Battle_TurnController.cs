7fce119e76ab0f0eb35a22f1887d229641e91263
wasteproduct <heilmittelh@naver.com>
2018-10-06 19:04:21 +0900

    181006_1

---
+++ Portfolio/Assets/Scripts/Battle_TurnController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Battle
+{
+    [CreateAssetMenu(fileName = "", menuName = "Battle/Turn Controller", order = 1)]
+    public class Battle_TurnController : ScriptableObject
+    {
+        private int controllingNumber = 0;
+
+        public void Initialize()
+        {
+            controllingNumber = 0;
+        }
+
+        //여기
+    }
+}
---
+++ Portfolio/Assets/Scripts/Manager_Battle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+namespace Battle
+{
+    public class Manager_Battle : MonoBehaviour
+    {
+        public Player_Team playerTeam;
+        //public ENEMY enemies
+
+        private bool acting;
+
+        // temporary
+
+        public void Initialize()
+        {
+            acting = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (acting == true) return;
+
+            StartCoroutine(CharacterAction());
+        }
+
+        private void TurnOver()
+        {
+
+        }
+
+        private IEnumerator CharacterAction()
+        {
+            acting = true;
+
+            yield return null;
+        }
+    }
+}
