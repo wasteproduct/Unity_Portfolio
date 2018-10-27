fcbc1a7ed4d6a7141f92240f9561258a2dda23cd
wasteproduct <heilmittelh@naver.com>
2018-10-28 08:14:03 +0900

    181028_1

---
+++ Portfolio/Assets/Scripts/Tutorial/Tutorial_Camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tutorial
+{
+    public class Tutorial_Camera : MonoBehaviour
+    {
+        public GameObject focus;
+
+        private Vector3 offset;
+
+        // Use this for initialization
+        void Start()
+        {
+            offset = new Vector3(0, 1.6f, 3.2f);
+
+            this.transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            this.transform.position = focus.gameObject.transform.position + offset;
+        }
+    }
+}
---
+++ Portfolio/Assets/Scripts/Tutorial/Tutorial_SoldierAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tutorial
+{
+    public class Tutorial_SoldierAnimation : MonoBehaviour
+    {
+        private Animator animator;
+
+        private float horizontal;
+        private float vertical;
+
+        // Use this for initialization
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                animator.Play("combat_shoot_burst", -1, 0.0f);
+            }
+
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
+
+            animator.SetFloat("Horizontal", horizontal);
+            animator.SetFloat("Vertical", vertical);
+        }
+    }
+}
