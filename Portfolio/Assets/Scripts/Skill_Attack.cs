e3e510db1173f287f282ef6009ada7b1758b9278
wasteproduct <heilmittelh@naver.com>
2018-11-08 17:24:03 +0900

    181108_1

---
+++ Portfolio/Assets/Scripts/Skill_Attack.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Skill
+{
+    [CreateAssetMenu(fileName = "", menuName = "Skill/Attack", order = 1)]
+    public class Skill_Attack : Skill_Base
+    {
+
+    }
+}
---
+++ Portfolio/Assets/Scripts/Skill_Base.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Skill
+{
+    public abstract class Skill_Base : ScriptableObject
+    {
+        public GameObject Target { get; protected set; }
+    }
+}
