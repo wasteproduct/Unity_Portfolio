594f12dc62de3b7f6128772e1b2a855b82412323
wasteproduct <heilmittelh@naver.com>
2018-11-14 16:13:35 +0900

    181114_3

---
+++ Portfolio/Assets/Scripts/Tutorial/Tutorial_AnimatorTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tutorial
+{
+    public class Tutorial_AnimatorTest : MonoBehaviour
+    {
+        private Animator animator;
+
+        // Use this for initialization
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+
+            animator.SetInteger("CurrentState", 2);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                animator.SetBool("grenade", true);
+            }
+        }
+    }
+}
