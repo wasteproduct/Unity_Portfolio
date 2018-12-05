1c04d58c05d9015be9ec4898f04c8bb635363c43
wasteproduct <heilmittelh@naver.com>
2018-12-05 19:42:15 +0900

    181205_4

---
+++ Portfolio/Assets/Scripts/FogOfWar_Manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogOfWar_Manager : MonoBehaviour
+{
+    [SerializeField]
+    private int textureSize = 256;
+    [SerializeField]
+    private Color fogOfWarColor;
+    [SerializeField]
+    private LayerMask fogOfWarLayer;
+
+    private Texture2D fogOfWarTexture;
+    private Color[] pixels;
+    private List<FogOfWar_Revealer> revealers;
+    private int pixelsPerUnit;
+    private Vector2 centerPixel;
+
+    public static FogOfWar_Manager Instance { get; private set; }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
