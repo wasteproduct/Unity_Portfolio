c2180ef1d225a893c91a0ef68f758f30d4946cd6
wasteproduct <heilmittelh@naver.com>
2018-12-27 13:05:04 +0900

    181227_1

--- Portfolio/Assets/Scripts/Tutorial/Tutorial_BinarySpacePartitioning.cs
+++ Portfolio/Assets/Scripts/Tutorial/Tutorial_BinarySpacePartitioning.cs
@@ -6,25 +6,47 @@ namespace Tutorial
 {
     public class MapArea
     {
-        public MapArea(int x, int z, int dividedDirection)
-        {
-            //StartingX = startX;
-            //StartingZ = startZ;
-            //EndX = endX;
-            //EndZ = endZ;
-            X = x;
-            Z = z;
+        private const int vertical = 0;
+        private const int horizontal = 1;
+        private const int areaSize = 32;
 
-            DividedDirection = dividedDirection;
+        public MapArea(int startX, int startZ, int endX, int endZ, int loopCount)
+        {
+            StartX = startX;
+            StartZ = startZ;
+            EndX = endX;
+            EndZ = endZ;
+
+            Children = null;
+
+            if (loopCount > 3) return;
+
+            Children = new MapArea[2];
+
+            int dividingDirection = ((EndZ - StartZ) > (EndX - StartX)) ? horizontal : vertical;
+
+            switch (dividingDirection)
+            {
+                case vertical:
+                    int dividingX = Random.Range(StartX + areaSize, EndX - areaSize);
+
+                    Children[0] = new MapArea(StartX, StartZ, dividingX, EndZ, loopCount + 1);
+                    Children[1] = new MapArea(dividingX + 1, StartZ, EndX, EndZ, loopCount + 1);
+                    break;
+                case horizontal:
+                    int dividingZ = Random.Range(StartZ + areaSize, EndZ - areaSize);
+
+                    Children[0] = new MapArea(StartX, StartZ, EndX, dividingZ, loopCount + 1);
+                    Children[1] = new MapArea(StartX, dividingZ + 1, EndX, EndZ, loopCount + 1);
+                    break;
+            }
         }
 
-        public int X { get; private set; }
-        public int Z { get; private set; }
-        //public int StartingX { get; private set; }
-        //public int StartingZ { get; private set; }
-        //public int EndX { get; private set; }
-        //public int EndZ { get; private set; }
-        public int DividedDirection { get; private set; }
+        public int StartX { get; private set; }
+        public int StartZ { get; private set; }
+        public int EndX { get; private set; }
+        public int EndZ { get; private set; }
+        public MapArea[] Children { get; private set; }
     }
 
     public class Tutorial_BinarySpacePartitioning : MonoBehaviour
@@ -42,11 +64,8 @@ namespace Tutorial
             // x 0 - 128, z 0 - 128
             // min x, min z 32
 
-
-
-
-
             // horiz / vert random
+            MapArea mapArea = new MapArea(0, 0, 128, 128, 0);
         }
 
         // Update is called once per frame
