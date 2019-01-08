dfd9ba44ba52660faaf8a302402d290cec382c02
wasteproduct <heilmittelh@naver.com>
2019-01-08 19:41:22 +0900

    190108_3
    saving fbm

--- Portfolio/Assets/Scripts/Battle_ImpactBigBullyTomahawk.cs
+++ Portfolio/Assets/Scripts/Battle_ImpactBigBullyTomahawk.cs
@@ -7,9 +7,24 @@ namespace Battle
     [CreateAssetMenu(fileName = "", menuName = "Attack Action Impact/Big Bully Tomahawk", order = 1)]
     public class Battle_ImpactBigBullyTomahawk : Battle_ImpactBase
     {
+        public Battle_TurnController turnController;
+        public Battle_TargetManager targetManager;
+
+        [SerializeField]
+        private GameObject chicken;
+
         public override void Run()
         {
+            Transform bullyTransform = turnController.CurrentTurnCharacter.gameObject.transform;
 
+            Vector3 targetPosition = targetManager.FinalTargets[0].gameObject.transform.position + Vector3.up;
+
+            GameObject tomahawk = Instantiate(chicken, bullyTransform.position + Vector3.up, bullyTransform.rotation);
+
+            Vector3 force = targetPosition - tomahawk.transform.position;
+            force *= 2.0f;
+
+            tomahawk.gameObject.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
         }
     }
 }
