8e5729b860d5ffd97d3983b1573a165d24697df4
wasteproduct <heilmittelh@naver.com>
2019-01-16 18:58:10 +0900

    190116_1

--- Portfolio/Assets/Scripts/UI_CharacterSlot_ComposingTeam.cs
+++ Portfolio/Assets/Scripts/UI_CharacterSlot_ComposingTeam.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Character;
+using UnityEngine.UI;
 
-public class UI_CharacterSlot_ComposingTeam : MonoBehaviour {
+public class UI_CharacterSlot_ComposingTeam : MonoBehaviour
+{
+    [SerializeField]
+    private Image highlightFrame;
+    [SerializeField]
+    private Image draftedSign;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Character_Base SlotCharacter { get; private set; }
+    public bool Drafted { get; private set; }
+
+    public void SelectSlot()
+    {
+        if (Drafted == true)
+        {
+            SetDrafted(false);
+        }
+        else
+        {
+            HighlightSlot(true);
+        }
+    }
+
+    public void HighlightSlot(bool flag) { highlightFrame.gameObject.SetActive(flag); }
+
+    public void SetDrafted(bool flag)
+    {
+        draftedSign.gameObject.SetActive(flag);
+        Drafted = flag;
+    }
+
+    public void SetSlot(Character_Base slotCharacter)
+    {
+        SlotCharacter = slotCharacter;
+
+        GetComponent<Image>().sprite = SlotCharacter.Portrait;
+
+        SetDrafted(false);
+        HighlightSlot(false);
+    }
 }
--- Portfolio/Assets/Scripts/UI_Window_ComposeTeam.cs
+++ Portfolio/Assets/Scripts/UI_Window_ComposeTeam.cs
@@ -10,24 +10,24 @@ public class UI_Window_ComposeTeam : UI_Window_Base
     [SerializeField]
     private Player_Main playerMain;
     [SerializeField]
-    private UI_CharacterSlot[] characterSlots;
+    private UI_CharacterSlot_ComposingTeam[] characterSlots;
 
     public void SelectSlot()
     {
-        for (int i = 0; i < characterSlots.Length; i++)
-        {
-            if (characterSlots[i].gameObject.activeSelf == false) continue;
+        //for (int i = 0; i < characterSlots.Length; i++)
+        //{
+        //    if (characterSlots[i].gameObject.activeSelf == false) continue;
 
-            characterSlots[i].HighlightSlot(false);
+        //    characterSlots[i].HighlightSlot(false);
 
-            if (characterSlots[i].SlotCharacter.InstantiatedModel == null) continue;
-            characterSlots[i].SlotCharacter.InstantiatedModel.gameObject.SetActive(false);
-        }
+        //    if (characterSlots[i].SlotCharacter.InstantiatedModel == null) continue;
+        //    characterSlots[i].SlotCharacter.InstantiatedModel.gameObject.SetActive(false);
+        //}
     }
 
     public void Editor_GetSlots()
     {
-        UI_CharacterSlot[] slots = GetComponentsInChildren<UI_CharacterSlot>(true);
+        UI_CharacterSlot_ComposingTeam[] slots = GetComponentsInChildren<UI_CharacterSlot_ComposingTeam>(true);
 
         for (int i = 0; i < slots.Length; i++)
         {
